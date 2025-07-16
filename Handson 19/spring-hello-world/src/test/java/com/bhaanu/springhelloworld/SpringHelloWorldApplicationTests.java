3f2a5c3674967a0959d937432cacb272b4b74bce
Bhavani14SA <bhavanigowda117@gmail.com>
2025-07-16 10:12:14 +0530

    adding week 4 handson 19
    Hello World RESTful Web Service

---
+++ Handson 19/spring-hello-world/src/test/java/com/bhaanu/springhelloworld/SpringHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhaanu.springhelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
